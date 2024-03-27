135f8777edecd064c5f2e786120d79c379ac1264
Mohammed <6bouass9@gmail.com>
2024-03-27 01:52:11 +0000

    add print_file

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,11 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+
 /**
  *  * struct binary_tree_s - Binary tree node
  *   *
