b68a2c3c9ea4c438e044b5aae9b2fa8de35f766e
Rafiki Mostafa <rafikimostafa@gmail.com>
2024-03-29 03:13:18 +0000

    Task 28 v0.2

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -20,7 +20,12 @@ bst_t *bst_remove(bst_t *root, int value)
 		root->right = bst_remove(root->right, value);
 	else
 	{
-		if (!root->right)
+    if (!root->left && !root->right)
+    {
+      free(root);
+      return (NULL);
+    }
+    else if (!root->right)
 		{
 			bstree = root->left;
 			free(root);
